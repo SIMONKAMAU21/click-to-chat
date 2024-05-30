7ad6d8942b0e84600c20228a5ce6a10976733e42
simon kamau <https://github.com/SIMONKAMAU21/own-git-.git>
2024-05-30 12:26:06 +0300

    added the nav bar and pages that is (about and home page)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 
+import { Box } from '@chakra-ui/react'
 import './App.css'
+import Navbar from './layouts/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
+  const appStyles={
+with:'100vw',
+h:"100vh"
+  }
 
   return (
     <>
-      app
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route exact path='/' element={<Home/>}/>
+      <Route path='/About' element={<About/>} />
+    </Routes>
+    </BrowserRouter>
+    <Box sx={appStyles}>
+    </Box>
     </>
   )
 }
---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Box, Flex, HStack, Link, IconButton, useDisclosure, useColorModeValue, Stack } from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems="center" justifyContent="space-between">
+        <IconButton
+          size="md"
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label="Open Menu"
+          display={{ md: 'none' }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack spacing={8}  justifyContent={{base:'none' , md:'space-between'}} w='100%' alignItems="center" >
+          <Box fontWeight='bold'>Click to Chat</Box>
+          <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }} bg='yellow'>
+            <NavLink to="/" style={{ textDecoration: 'none' }} activeStyle={{ fontWeight: 'bold' }}>
+              Home
+            </NavLink>
+            <NavLink to="/about" style={{ textDecoration: 'none' }} activeStyle={{ fontWeight: 'bold' }}>
+              About
+            </NavLink>
+          </HStack>
+        </HStack>
+      </Flex>
+
+      {isOpen ? (
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack as="nav" spacing={4}>
+            <NavLink to="/" style={{ textDecoration: 'none' }} activeStyle={{ fontWeight: 'bold' }}>
+              Home
+            </NavLink>
+            <NavLink to="/about" style={{ textDecoration: 'none' }} activeStyle={{ fontWeight: 'bold' }}>
+              About
+            </NavLink>
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
+  );
+};
+
+export default NavBar;
