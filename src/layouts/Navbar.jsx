3a182a1d82b0d4db55fead569a1793a75e7d09b5
simon kamau <https://github.com/SIMONKAMAU21/own-git-.git>
2024-05-30 20:20:32 +0300

    modified all pages to be more resposive

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, HStack, Link, IconButton, useDisclosure, useColorModeValue, Stack } from '@chakra-ui/react';
+import { Box, Flex, HStack, IconButton, useDisclosure, useColorModeValue, Stack, Spacer } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import { NavLink } from 'react-router-dom';
 
@@ -7,35 +7,36 @@ const NavBar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Box bg={useColorModeValue('green.300', 'green.900')} px={4} position='fixed' zIndex='1000' width={{base:"100vw", md:"100vw"}}>
+    <Box boxShadow='lg' bg={useColorModeValue('green.300', 'green.900')} px={4} position='fixed' zIndex='1000' width={{ base: "100vw", md: "100vw" }}>
       <Flex h={16} alignItems="center" justifyContent="space-between">
-        <IconButton
-          size="md"
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label="Open Menu"
-          display={{ md: 'none' }}
-          onClick={isOpen ? onClose : onOpen}
-        />
-        <HStack spacing={8}  justifyContent={{base:'none' , md:'space-between'}} w='100%' alignItems="center" >
+        <HStack spacing={8}>
+          <IconButton
+            size="md"
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label="Open Menu"
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
           <Box fontWeight='bold'>Click to Chat</Box>
-          <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }} >
-            <NavLink to="/" style={{ textDecoration: 'none' }} activeStyle={{ fontWeight: 'bold' }}>
-              Home
-            </NavLink>
-            <NavLink to="/about" style={{ textDecoration: 'none' }} activeStyle={{ fontWeight: 'bold' }}>
-              About
-            </NavLink>
-          </HStack>
+        </HStack>
+        <Spacer />
+        <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }}>
+          <NavLink to="/" style={{ textDecoration: 'none', fontWeight: 'bold' }}>
+            Home
+          </NavLink>
+          <NavLink to="/about" style={{ textDecoration: 'none', fontWeight: 'bold' }}>
+            About
+          </NavLink>
         </HStack>
       </Flex>
 
       {isOpen ? (
         <Box pb={4} display={{ md: 'none' }}>
           <Stack as="nav" spacing={4}>
-            <NavLink to="/" style={{ textDecoration: 'none' }} activeStyle={{ fontWeight: 'bold' }}>
+            <NavLink to="/" style={{ textDecoration: 'none', fontWeight: 'bold' }} onClick={onClose}>
               Home
             </NavLink>
-            <NavLink to="/about" style={{ textDecoration: 'none' }} activeStyle={{ fontWeight: 'bold' }}>
+            <NavLink to="/about" style={{ textDecoration: 'none', fontWeight: 'bold' }} onClick={onClose}>
               About
             </NavLink>
           </Stack>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,51 @@
 import React, { useState } from 'react';
-import { Box, Button, Heading, Text, VStack } from '@chakra-ui/react';
+import { Box, Button, Spinner, Heading, Text, VStack } from '@chakra-ui/react';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 
 const Home = () => {
    const [phone, setPhone] = useState('');
    const [errorMessage, setErrorMessage] = useState('');
+   const [loading,setLoading]=useState(false)
 
    const handleChange = (value) => {
       setPhone(value);
    };
 
    const handleClick = () => {
+      setLoading(true)
       const phoneNumber = phone.replace(/\D/g, '');
       if (phoneNumber === '') {
          setErrorMessage('Please enter a valid phone number');
+         setLoading(false)
       } else {
          setErrorMessage('');
-         const url = `https://wa.me/${phoneNumber}`;
-         window.open(url, '_blank');
+         setTimeout(()=>{
+            const url = `https://wa.me/${phoneNumber}`;
+            window.open(url, '_blank');
+            setLoading(false)
+         },2000)
       }
    };
 
    return (
       <Box p={4} display="flex" alignItems="center" justifyContent="center" h="100vh" w="100vw" position="fixed" backgroundSize="cover"
-         backgroundPosition="center" opacity="0.8" backgroundImage="url('/path/to/your/WhatsApp.jpg')"
+         backgroundPosition="center" boxShadow='10px 10px 5px 5px rgba(0,0,0,0.5)' backgroundImage="url('/path/to/your/WhatsApp.jpg')"
       >
          <VStack spacing={4} w="80%" bg="green.300" h="80%" display="flex" justifyContent="space-around" borderRadius="lg" boxShadow="lg" p={6}>
             <Heading color="white">Click to Chat</Heading>
+            <Text color="white" textAlign="center" mb={4}>
+               Enter your phone number below and click the button to start a WhatsApp chat.
+            </Text>
             <VStack display="flex" justifyContent="space-around" h="60%">
                <PhoneInput
                   country={'ke'}
                   value={phone}
                   onChange={handleChange}
                />
-               <Button bg="green.500" color="white" onClick={handleClick}>Open WhatsApp</Button>
+               <Button bg="white" color="green" onClick={handleClick}>
+                  {loading?<Spinner color='green' size='lg' thickness='5px'/>:" Open Whatsapp"}
+                  </Button>
                {errorMessage && <Text color="red.500">{errorMessage}</Text>}
             </VStack>
          </VStack>
